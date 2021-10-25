cac48de99706c698e44e5f8409fcd471ada633fd
perpet99 <perpet99@gmail.com>
2021-10-25 17:01:17 +0900

    initialize code

---
+++ wwwroot/JavaScript.js
@@ -0,0 +1,25 @@
+﻿
+
+
+function start()
+{
+    // Your access token can be found at: https://cesium.com/ion/tokens.
+    // This is the default access token from your ion account
+
+    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ZjI1MDQ4My1hNTdkLTQ4YmMtYjZlYi1iNDkxMmEwY2FmMjkiLCJpZCI6Njg5MzksImlhdCI6MTYzMzQxMzk2OH0.Oo_93Wrky-2P2zI434flx68KdBWE_-yStvSW6erPG8s';
+
+    // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
+    const viewer = new Cesium.Viewer('cesiumContainer', {
+        terrainProvider: Cesium.createWorldTerrain()
+    });
+    // Add Cesium OSM Buildings, a global 3D buildings layer.
+    const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());
+    // Fly the camera to San Francisco at the given longitude, latitude, and height.
+    viewer.camera.flyTo({
+        destination: Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+        orientation: {
+            heading: Cesium.Math.toRadians(0.0),
+            pitch: Cesium.Math.toRadians(-15.0),
+        }
+    });
+}
