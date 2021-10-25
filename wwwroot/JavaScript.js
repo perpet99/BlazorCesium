e67a9e9e4ea474e56fbe51fe46101c50ab467a4f
perpet99 <perpet99@gmail.com>
2021-10-25 17:15:49 +0900

    initialize code

--- wwwroot/JavaScript.js
+++ wwwroot/JavaScript.js
@@ -6,7 +6,7 @@ function start()
     // Your access token can be found at: https://cesium.com/ion/tokens.
     // This is the default access token from your ion account
 
-    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI4ZjI1MDQ4My1hNTdkLTQ4YmMtYjZlYi1iNDkxMmEwY2FmMjkiLCJpZCI6Njg5MzksImlhdCI6MTYzMzQxMzk2OH0.Oo_93Wrky-2P2zI434flx68KdBWE_-yStvSW6erPG8s';
+    Cesium.Ion.defaultAccessToken = 'add your token';
 
     // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
     const viewer = new Cesium.Viewer('cesiumContainer', {
